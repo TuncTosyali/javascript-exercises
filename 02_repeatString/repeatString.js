febb0fe31cb1a1b2742fd0655b586ebb518991cd
TuncTosyali <httosyali@gmail.com>
2025-03-01 23:59:44 +0300

    complete exercise 2 repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, count) {
+    let finalstr='';
+    if(count < 0){
+        return "ERROR";
+    }
+    for(let i=0; i<count; i++){
+        finalstr+=str;
+    }
+    return finalstr;
 };
 
 // Do not edit below this line
