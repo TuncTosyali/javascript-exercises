5e0793ae319789e210cbcee57c19592ebb2b6c7d
TuncTosyali <httosyali@gmail.com>
2025-03-02 15:52:15 +0300

    complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function (inStr) {
+    let outStr = "";
+    for (let i = 1; inStr.length - i >= 0; i++) {
+        outStr += inStr.at(inStr.length - i);
+    }
+    //console.log(outStr);
+    return outStr;
 };
+
+//reverseString("123");
 
 // Do not edit below this line
 module.exports = reverseString;
