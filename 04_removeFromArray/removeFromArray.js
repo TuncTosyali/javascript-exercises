995ca6e1d1bea2348606e9544a162c2ce3ec9113
TuncTosyali <httosyali@gmail.com>
2025-03-03 10:34:48 +0300

    complete exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function (inArray, ...toBeRemoved) {
+    let outArray = [];
+    let clearFlag = true;
+    for (let i = 0; i < inArray.length; i++) {
+        for (let r = 0; r < toBeRemoved.length; r++) {
+            if (inArray.at(i) === toBeRemoved.at(r)) {
+                clearFlag = false;
+            }
+        }
+        if (clearFlag) {
+            outArray.push(inArray.at(i));
+        }
+        else {
+            clearFlag = true;
+        }
+    }
+    return outArray;
+    //console.log("myline");
 };
+
+//removeFromArray();
 
 // Do not edit below this line
 module.exports = removeFromArray;
