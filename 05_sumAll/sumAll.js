04258aa0471ed2cd180c70072fe1c64d0760fd59
TuncTosyali <httosyali@gmail.com>
2025-03-03 11:42:26 +0300

    complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function (inSmallerNum, inLargerNum) {
+    if (!Number.isInteger(inSmallerNum) || !Number.isInteger(inLargerNum)) return "ERROR";
+    if (inSmallerNum < 0 || inLargerNum < 0) return "ERROR";
+    //rotate parameter between each other if they are provided in wrong order
+    if(inSmallerNum > inLargerNum){
+        let temp=inSmallerNum;
+        inSmallerNum=inLargerNum;
+        inLargerNum=temp;
+    }//---------------------------
+    let toBeReturned=0;
+    for(let i=inSmallerNum; i<=inLargerNum;i++){
+        toBeReturned+=i;
+    }
+    return toBeReturned;
 };
 
 // Do not edit below this line
