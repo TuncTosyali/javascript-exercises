88b9e262b9ee52ceb74e5b7e3305f51ed533d871
TuncTosyali <httosyali@gmail.com>
2025-03-03 12:14:36 +0300

    complete exercise 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(inYear) {
+    if(inYear%4==0){
+        if(inYear%100==0){
+            if(inYear%400==0) return true;
+            else return false;
+        }
+        else return true;
+    }
+    else return false;
 };
+
+//leapYears(1996);
 
 // Do not edit below this line
 module.exports = leapYears;
