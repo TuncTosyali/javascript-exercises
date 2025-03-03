208874136269ae3215fb2a4f39c934d44e206e2b
TuncTosyali <httosyali@gmail.com>
2025-03-03 12:49:15 +0300

    complete exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (inFah) {
+  let outCel=Math.round(((inFah-32)*(5/9))*10)/10;
+  return outCel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (inCel) {
+  let outFah=Math.round(((inCel*(9/5))+32)*10)/10;
+  return outFah;
 };
+
+//convertToCelsius(65);
 
 // Do not edit below this line
 module.exports = {
