97f6c4b57c0515cc7ed106ab7dc6709292eef7fd
TuncTosyali <httosyali@gmail.com>
2025-03-11 12:09:31 +0300

    complete exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function (inNum) {
+    let realNum = Number(inNum);
+    if (!Number.isInteger(realNum) || !(realNum >= 0)) return "OOPS";
+    if (realNum === 0){
+        return 0;
+    } else if(realNum === 1){
+        return 1;
+    } else {
+        return fibonacci(realNum - 1) + fibonacci(realNum - 2);
+    }
 };
+
+console.log(fibonacci(4));
+//console.log(Number.isInteger(-25));
+
 
 // Do not edit below this line
 module.exports = fibonacci;
